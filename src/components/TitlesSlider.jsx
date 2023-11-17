32a77b73e6f2b9bc8b8ddca9c00b33abecf487da
Pasha Rezero <kivachuk.2018@gmail.com>
2023-11-17 19:57:10 +0200

    combine 2 useEffect

--- src/components/TitlesSlider.jsx
+++ src/components/TitlesSlider.jsx
@@ -3,45 +3,18 @@ import TinderCard from "react-tinder-card";
 import styles from "./TitlesSlider.module.css";
 import axios from "axios";
 
-
-
 const TitlesSlider = () => {
   const BASE_URL = "https://api.jikan.moe/v4/top/anime";
-  const randomPage = Math.floor(Math.random() * 1000) + 1;
 
   const [lastDirection, setLastDirection] = useState();
   const [dataDisplay, setDataDisplay] = useState([]);
   const [swipedCards, setSwipedCards] = useState(0);
-  const [canSwipe, setCanSwipe] = useState(true);
-
 
   useEffect(() => {
     if (swipedCards === dataDisplay.length) {
-      axios
-      .get(`${BASE_URL}?page=${randomPage}`)
-      .then((response) => {
-          if (response.status !== 200) {
-            throw new Error("Network response was not ok");
-          }
-          return response.data.data;
-        })
-        .then((data) => {
-          const newDataDisplay = data.map((elem) => ({
-            id: elem.mal_id,
-            image: elem.images.jpg.large_image_url,
-            name: elem.title,
-          }));
-          setDataDisplay(newDataDisplay);
-          setSwipedCards(0);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }
-  }, [swipedCards, dataDisplay.length]);
+      const randomPage = Math.floor(Math.random() * 1000) + 1;
+      console.log('Making API request with random page:', randomPage);
   
-  useEffect(() => {
-    if (swipedCards === dataDisplay.length && canSwipe) {
       axios
         .get(`${BASE_URL}?page=${randomPage}`)
         .then((response) => {
@@ -57,18 +30,17 @@ const TitlesSlider = () => {
               image: elem.images.jpg.large_image_url,
               name: elem.title,
             }));
-
             setDataDisplay((prevData) => [...prevData, ...newDataDisplay]);
-            setSwipedCards(0); 
+            setSwipedCards(0);
           } else {
-            setCanSwipe(false);
+            
           }
         })
         .catch((error) => {
           console.error(error);
         });
     }
-  }, [swipedCards, dataDisplay.length, canSwipe]);
+  }, [swipedCards, dataDisplay.length]);
 
   const swiped = (direction, nameToDelete) => {
     setLastDirection(direction);
@@ -78,35 +50,34 @@ const TitlesSlider = () => {
     setDataDisplay((prevData) => prevData.filter((elem) => elem.name !== nameToDelete));
   };
 
-  const swipe = async (dir) => {
-    if (canSwipe && currentIndex < dataDisplay.length) {
-      await childRefs[currentIndex].current.swipe(dir)  //Swipe не юзается почему то
-    }
-  }
-
   const outOfFrame = (name) => {
-    // console.log(name + " left the screen!"); //ХЗ надо это ил нет вообще
+    console.log(name + " left the screen!");
   };
 
- return (
-  <div >
-   <div className={styles.cardContainer} >
-   {dataDisplay.map((elem) =>
-          <TinderCard className={styles.swipe} key={elem.id} onSwipe={(dir) => swiped(dir, elem.name)} onCardLeftScreen={() => outOfFrame(elem.name)}>
+  return (
+    <div>
+      <div className={styles.cardContainer}>
+        {dataDisplay.map((elem) => (
+          <TinderCard
+            className={styles.swipe}
+            key={elem.id}
+            onSwipe={(dir) => swiped(dir, elem.name)}
+            onCardLeftScreen={() => outOfFrame(elem.name)}
+          >
             <div style={{ backgroundImage: 'url(' + elem.image + ')' }} className={styles.card}>
               <h3>{elem.name}</h3>
             </div>
           </TinderCard>
-        )}
-   </div>
+        ))}
+      </div>
 
-   {lastDirection ? (
-    <h2 className={styles.infoText}>You swiped {lastDirection}</h2>
-   ) : (
-    <h2 className={styles.infoText} />
-   )}
-  </div>
- );
+      {lastDirection ? (
+        <h2 className={styles.infoText}>You swiped {lastDirection}</h2>
+      ) : (
+        <h2 className={styles.infoText} />
+      )}
+    </div>
+  );
 };
 
 export default TitlesSlider;
