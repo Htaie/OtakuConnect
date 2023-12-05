7c651726477173bdb284e057f4f62e74122f198c
DruidPro <hataaiee@gmail.com>
2023-12-05 23:19:05 +0500

    testing

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,28 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true,"node": true,},
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
-    
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import'], // Добавлен плагин import
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
-      
     ],
     'import/no-unresolved': 'error',
     'import/no-unused-modules': 'error',
-    "semi": ["error", "always"],
-    "no-console": "warn",
-    'camelcase': ['error', { 'properties': 'always' }],
-    'indent': ['error', 2],
-    'eqeqeq': 'error',
-    'quotes': ['error', 'single'],
-
+    semi: ['error', 'always'],
+    'no-console': 'warn',
+    camelcase: ['error', { properties: 'always' }],
+    indent: ['error', 2],
+    eqeqeq: 'error',
+    quotes: ['error', 'single'],
   },
-}
+};
