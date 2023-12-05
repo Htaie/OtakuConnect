d4808d1b70c6f4936e4bc5bc0faee2d60bc1a43b
DruidPro <hataaiee@gmail.com>
2023-12-05 23:43:52 +0500

    sd32

--- src/pages/SecondPAge.jsx
+++ src/pages/SecondPAge.jsx
@@ -17,7 +17,9 @@ const SecondPAge = () => {
  useEffect(() => {
   const newSocket = io("http://89.104.65.22", { transports: ["polling", "websocket"] });
   const kek =0 
-  const kek =0 
+  const kek =0
+  const kek =0
+  const kek =0
   newSocket.on("userConnected", (userId) => {
    console.log(`User connected: ${userId}`);
   });
