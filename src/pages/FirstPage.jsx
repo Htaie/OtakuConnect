c98a77db3c35dc31e50ad16a9b3a615d8ec0cb3e
DruidPro <hataaiee@gmail.com>
2023-12-05 23:08:45 +0500

    123

--- src/pages/FirstPage.jsx
+++ src/pages/FirstPage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Navbar from "../components/Navbar";
 import { Link, useNavigate } from "react-router-dom";
