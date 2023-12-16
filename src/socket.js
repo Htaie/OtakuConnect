3b02b396dd29cc343772ddbdddb5d56c46b314e6
Pasha Rezero <kivachuk.2018@gmail.com>
2023-12-16 21:29:22 +0200

    add registration

--- src/socket.js
+++ src/socket.js
@@ -2,9 +2,67 @@ import express from 'express'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
 import cors from 'cors'
+import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
+import { check, validationResult } from 'express-validator'
+import jwt from 'jsonwebtoken'
+import secret from './config.js'
+import User from './models/User.js'
+import Role from './models/Role.js'
+import roleMiddleware from './middleware/roleMiddleware.js'
 
 const app = express()
 app.use(cors())
+app.use(express.json())
+
+app.post('/login', async function (req, res) {
+  try {
+    await authCtrl.login(req, res)
+  } catch (e) {
+    console.error(e)
+    res.status(500).json({ error: 'Login Client Error' })
+  }
+})
+
+app.post(
+  '/register',
+  [
+    check('login', 'Логин пользователя не может быть пустым').notEmpty(),
+    check('username', 'Имя пользователя не может быть пустым').notEmpty(),
+    check(
+      'password',
+      'Пароль должен быть больше 4 и меньше 10 символов'
+    ).isLength({ min: 4, max: 10 }),
+  ],
+  async function (req, res) {
+    console.log('Request Body:', req.body)
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    try {
+      const { login, username, password } = req.body
+      console.log('Login:', login)
+      console.log('Name:', username)
+      console.log('Password:', password)
+      await authCtrl.registration(req, res)
+    } catch (e) {
+      console.error(e)
+      res.status(500).json({ error: 'Register Client Error' })
+    }
+  }
+)
+
+app.get('/users', roleMiddleware(['ADMIN']), async function (req, res) {
+  try {
+    await authCtrl.getUsers(req, res)
+  } catch (e) {
+    console.error(e)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
 
 const server = createServer(app)
 const io = new Server(server, {
@@ -125,3 +183,84 @@ function generateRandomNickname() {
 
   return randomAdjective + ' ' + randomNoun
 }
+
+const generateAccessToken = (id, roles) => {
+  const payload = {
+    id,
+    roles,
+  }
+  return jwt.sign(payload, secret, { expiresIn: '24h' })
+}
+
+async function connectToMongoDB() {
+  await mongoose.connect(
+    'mongodb+srv://Otaku:Otaku123@cluster0.bqlzraq.mongodb.net/auth_roles?retryWrites=true&w=majority'
+  )
+}
+
+connectToMongoDB()
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error)
+  })
+
+class authController {
+  async login(req, res) {
+    try {
+      const { login, password } = req.body
+      const user = await User.findOne({ login })
+      if (!user) {
+        return res
+          .status(400)
+          .json({ message: `Пользователь ${login} не найден` })
+      }
+      const validPassword = bcrypt.compareSync(password, user.password)
+      if (!validPassword) {
+        return res.status(400).json({ message: `Введен неверный пароль` })
+      }
+      const token = generateAccessToken(user._id, user.roles)
+      return res.json({ token, username: user.username })
+    } catch (e) {
+      console.log(e)
+      res.status(400).json({ message: 'Login error' })
+    }
+  }
+
+  async registration(req, res) {
+    try {
+      const { login, username, password } = req.body
+      const candidate = await User.findOne({ login })
+      if (candidate) {
+        return res
+          .status(400)
+          .json({ message: 'Пользователь с таким логином уже существует' })
+      }
+      const hashPassword = bcrypt.hashSync(password, 7)
+      const userRole = await Role.findOne({ value: 'USER' })
+      const user = new User({
+        login,
+        username,
+        password: hashPassword,
+        roles: [userRole.value],
+      })
+      await user.save()
+      return res.json({ message: 'Пользователь успешно зарегистрирован' })
+    } catch (e) {
+      console.log(e)
+      res.status(400).json({ message: 'Registration error' })
+    }
+  }
+
+  async getUsers(req, res) {
+    try {
+      const users = await User.find()
+      res.json(users)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+}
+
+const authCtrl = new authController()
