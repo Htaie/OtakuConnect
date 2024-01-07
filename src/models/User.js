ec43b047d08b62a4ef3606931e00ae7d38dbc072
Pasha Rezero <kivachuk.2018@gmail.com>
2024-01-07 12:16:56 +0200

    add image string to user model

--- src/models/User.js
+++ src/models/User.js
@@ -5,6 +5,7 @@ const UserSchema = new Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   roles: [{ type: String, ref: 'Role' }],
+  image: { type: String },
 });
 
 const User = model('User', UserSchema);
