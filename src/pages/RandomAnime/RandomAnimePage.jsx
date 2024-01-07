ec0b0a244bbd0f9f7fc7abc9091c4ee47073ab4b
Pasha Rezero <kivachuk.2018@gmail.com>
2024-01-07 16:10:38 +0200

    change styles for random page

--- src/pages/RandomAnime/RandomAnimePage.jsx
+++ src/pages/RandomAnime/RandomAnimePage.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import NavBar from '../../components/NavigationBar/Navbar';
+import StarOutlinedIcon from '@mui/icons-material/StarOutlined';
+
 const RandomAnimePage = () => {
   const [anime, setAnime] = useState([]);
   useEffect(() => {
@@ -19,24 +21,45 @@ const RandomAnimePage = () => {
   return (
     <>
       <NavBar />
-      <div className="text-white">
-        <h1>{anime.title}</h1>
-        <p>{anime.synopsis}</p>
-        <img
-          src={
-            anime.images?.jpg?.large_image_url ||
-            'https://avatanplus.com/files/resources/mid/590249aeb6e3a15bb0efd29d.png'
-          }
-          alt=""
-        />
-        <p>{anime.url}</p>
-        <p>{anime.duration}</p>
-        <p>{anime.eposodes}</p>
-        <p>{anime.rating}</p>
-        <p>{anime.score}</p>
-        <p>{anime.status}</p>
-        <p>{anime.source}</p>
-        <p>{anime.title_japanese}</p>
+      <div className="container mx-auto">
+        <div className="text-white flex">
+          <a href={anime.url} target="_blank" rel="noreferrer">
+            <img
+              src={
+                anime.images?.jpg?.large_image_url ||
+                'https://avatanplus.com/files/resources/mid/590249aeb6e3a15bb0efd29d.png'
+              }
+              alt=""
+              style={{ width: '252px', height: '352px' }}
+            />
+          </a>
+          <div className="flex flex-col ml-4">
+            <div className="flex items-center mb-2">
+              <StarOutlinedIcon className="text-yellow-500 size-6" />
+              <p className="text-2xl">{anime.score}</p>
+            </div>
+            <h1 className="text-3xl mt-4 mb-2">{anime.title}</h1>
+            <p className="text-1xl mt-3 mb-2">{anime.title_japanese}</p>
+            <div className="flex mt-4">
+              <div className="mt-3">
+                <p>Duration:</p>
+                <p>Episodes:</p>
+                <p>Rating:</p>
+                <p>Status:</p>
+              </div>
+              <div className="mt-3 ml-40">
+                <p>{anime.duration}</p>
+                <p>{anime.episodes}</p>
+                <p>{anime.rating}</p>
+                <p>{anime.status}</p>
+              </div>
+            </div>
+            <p className="mt-4">{anime.source}</p>
+          </div>
+        </div>
+        <p className="text-white mt-20" style={{ maxWidth: '600px' }}>
+          {anime.synopsis}
+        </p>
       </div>
     </>
   );
